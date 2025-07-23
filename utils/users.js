05fda163c086c47bed9e9f210065fc38ab1d5c94
Ourwillxyz <admin@ourwill.xyz>
2025-07-23 06:59:26 +0300

    Create users.js

---
+++ utils/users.js
@@ -0,0 +1,7 @@
+// This is a mock in-memory "database" for demo purposes only!
+const users = [
+  { id: 1, username: "admin", password: "admin123", isAdmin: true },
+  { id: 2, username: "user", password: "user123", isAdmin: false }
+];
+
+export default users;
